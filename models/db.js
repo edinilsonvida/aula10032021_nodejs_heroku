a1711ebaadf3854242def0503a6c5ab22956d654
Edinilson Vida <edinilson.vida@hotmail.com>
2021-03-31 12:14:21 -0300

    Atualização do db.js
    Atualização do db.js.

--- models/db.js
+++ models/db.js
@@ -1,7 +1,6 @@
 require('dotenv/config');
 const { Sequelize } = require('sequelize');
-const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, 
-    process.env.DB_PASSWORD, {
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     charset: 'utf8',
